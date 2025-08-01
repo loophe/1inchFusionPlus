8a7dcae673237c58f5d3b33f2f3e224659bbe45a
loophe <loophe1914@gmail.com>
2025-08-01 15:04:53 +0800

    0801_1504

---
+++ src/initChain.ts
@@ -0,0 +1,109 @@
+import {config} from './config'
+
+import {createServer, CreateServerReturnType} from 'prool'
+import assert from 'node:assert'
+// import {anvil} from 'prool/instances'
+
+import {
+    computeAddress,
+    ContractFactory,
+    JsonRpcProvider,
+    MaxUint256,
+    parseEther,
+    parseUnits,
+    randomBytes,
+    Wallet
+} from 'ethers'
+import factoryContract from './lib/ABI/EscrowFactory.json'
+import resolverContract from './lib/ABI/Resolver.json'
+import {Address} from '@1inch/cross-chain-sdk'
+
+export type ChainConfig = (typeof config.chain)['source' | 'destination']
+
+async function getProvider(cnf: ChainConfig): Promise<{provider: JsonRpcProvider}> {
+    // if (!cnf.createFork) {
+    //     return {
+    //         provider: new JsonRpcProvider(cnf.url, cnf.chainId, {
+    //             cacheTimeout: -1,
+    //             staticNetwork: true
+    //         })
+    //     }
+    // }
+
+    // const node = createServer({
+    //     instance: anvil({forkUrl: cnf.url, chainId: cnf.chainId}),
+    //     limit: 1
+    // })
+    // await node.start()
+
+    // const address = node.address()
+    // assert(address)
+
+    const provider = new JsonRpcProvider(`http://[::]:46335/1`, cnf.chainId, {
+        cacheTimeout: -1,
+        staticNetwork: true
+    })
+
+    return {
+        provider
+    
+    }
+}
+const resolverPk = '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a'
+
+export async function initChain(
+    cnf: ChainConfig
+): Promise<{node?: CreateServerReturnType; provider: JsonRpcProvider; escrowFactory: string; resolver: string}> {
+// ): Promise<{node?: CreateServerReturnType}> {
+    const {provider} = await getProvider(cnf)
+    const deployer = new Wallet(cnf.ownerPrivateKey, provider)
+    // console.log("console.log(Address.fromBigInt(0n).toString()) :"+Address.fromBigInt(0n).toString())
+    //deploy EscrowFactory
+    const escrowFactory = await deploy(
+        factoryContract,
+        [
+            cnf.limitOrderProtocol,
+            cnf.wrappedNative, // feeToken,
+            Address.fromBigInt(0n).toString(), // accessToken,
+            deployer.address, // owner
+            60 * 30, // src rescue delay
+            60 * 30 // dst rescue delay
+        ],
+        provider,
+        deployer
+    )
+    // // console.log(Address.fromBigInt(0n).toString())
+    console.log(`[${cnf.chainId}]`, `Escrow factory contract deployed to`, escrowFactory)
+
+    //deploy Resolver contract
+    const resolver = await deploy(
+        resolverContract,
+        [
+            escrowFactory,
+            cnf.limitOrderProtocol,
+            computeAddress(resolverPk) // resolver as owner of contract
+        ],
+        provider,
+        deployer
+    )
+    console.log(`[${cnf.chainId}]`, `Resolver contract deployed to`, resolver)
+    // return {node: node}
+
+    
+    return {provider, resolver, escrowFactory}
+}
+
+/**
+ * Deploy contract and return its address
+ */
+async function deploy(
+    json: {abi: any; bytecode: any},
+    params: unknown[],
+    provider: JsonRpcProvider,
+    deployer: Wallet
+): Promise<string> {
+    const deployed = await new ContractFactory(json.abi, json.bytecode, deployer).deploy(...params)
+    await deployed.waitForDeployment()
+
+    return await deployed.getAddress()
+}
---
+++ src/main.ts
@@ -0,0 +1,124 @@
+// import Sdk from '@1inch/cross-chain-sdk'
+import { createOrder } from './lib/create-order';
+import {Wallet} from './wallet';
+import {ChainConfig, config} from './config'
+import {Resolver} from './resolver'
+import {initChain} from './initChain';
+// import {anvil} from 'prool/instances'
+import {
+    computeAddress,
+    ContractFactory,
+    JsonRpcProvider,
+    MaxUint256,
+    parseEther,
+    parseUnits,
+    randomBytes,
+    Wallet as SignerWallet,
+    Provider
+} from 'ethers'
+
+import {uint8ArrayToHex, UINT_40_MAX} from '@1inch/byte-utils'
+
+import {  
+    HashLock,  
+    TimeLocks,
+    NetworkEnum,  
+    OrderStatus,  
+    PresetEnum,  
+    PrivateKeyProviderConnector,  
+    SDK,
+    CrossChainOrder ,
+    randBigInt,
+    Address,
+    AuctionDetails,
+    TakerTraits,
+    AmountMode
+} from '@1inch/cross-chain-sdk' 
+// import { time } from 'console'
+
+
+// console.log(NetworkEnum.BINANCE)
+const secret = uint8ArrayToHex(randomBytes(32))
+console.log('secret', secret)
+
+async function main() {
+ 
+	const userPk = '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'
+	const srcChainId = NetworkEnum.ETHEREUM;
+	const dstChainId = NetworkEnum.POLYGON;
+	type Chain = {
+        // node?: CreateServerReturnType | undefined
+        provider: JsonRpcProvider
+        escrowFactory: string
+        resolver: string
+    }
+
+    let src: Chain
+    let dst: Chain
+    // init 1/
+	;[src] = await Promise.all([initChain(config.chain.source)])
+	const srcChainUser = new Wallet(userPk, src.provider)
+    // get 1000 USDC for user in SRC chain and approve to LOP
+    await srcChainUser.topUpFromDonor(
+        config.chain.source.tokens.USDC.address,
+        config.chain.source.tokens.USDC.donor,
+        parseUnits('1000', 6)
+    )
+
+    const balace = await srcChainUser.tokenBalance(config.chain.source.tokens.USDC.address)
+    console.log(`User ${await srcChainUser.getAddress()} balance:`, balace.toString())
+    // approve LOP to spend USDC
+    console.log('Approving LOP to spend USDC...')
+    await srcChainUser.approveToken(
+        config.chain.source.tokens.USDC.address,
+        config.chain.source.limitOrderProtocol,
+        MaxUint256
+    )
+    // create_order 2/
+    const order = await createOrder(
+			'EVM',
+			await srcChainUser.getAddress(),
+			new Address("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"),
+			new Address("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"),
+			srcChainId,
+			dstChainId,
+			secret
+		);
+
+    // console.log(`\n🎈`, order);
+
+    const orderHash = order.getOrderHash(1)
+    // console.log('orderHash :', orderHash)
+
+    // SIGN ORDER 3/
+    const signature = await srcChainUser.signOrder(srcChainId, order);
+    // console.log("User signature :"+signature)
+
+    console.log('🎈', signature, orderHash);
+
+
+    //fill order 4/
+    const resolverPk = '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a'
+    const srcChainResolver = new Wallet(resolverPk, src.provider)
+    const fillAmount = order.makingAmount
+    const resolverContract = new Resolver(src.resolver)
+    const {txHash: orderFillHash, blockHash: srcDeployBlock} = await srcChainResolver.send(//1/2
+        resolverContract.deploySrc(
+            srcChainId,
+            order,
+            signature,
+            TakerTraits.default()
+                .setExtension(order.extension)
+                .setAmountMode(AmountMode.maker)
+                .setAmountThreshold(order.takingAmount),
+            fillAmount
+        )
+    )
+
+            console.log(`[${srcChainId}]`, `Order ${orderHash} filled for ${fillAmount} in tx ${orderFillHash}`)
+// }).catch((error) => {
+//     console.error('Error fetching block number:', error)
+// })
+
+}
+main().catch(console.error);
